d4f2f4e46373cf8f09cca7211c0b924f244985af
Digbijaya Lenka <digbijayalenka@gmail.com>
2025-03-28 05:35:06 +0530

    add Home component with background video and update styling

--- developeleaf_front_end/src/App.jsx
+++ developeleaf_front_end/src/App.jsx
@@ -1,17 +1,16 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+// components
+import Home from "./components/Home/Home";
 
 function App() {
-  
-
   return (
-    <div className='text-center mt-10 mb-10 bg-slate-200'>
-      <h1 className='text-3xl font-bold'>Develop Leaf</h1>
-      <h2 className='text-2xl font-bold'>Lets get started.</h2>
+    <div className="bg-transparent">
+      <Home />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ developeleaf_front_end/src/components/Home/Home.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="relative h-screen flex items-center justify-center text-center text-white">
+      {/* Background Video */}
+      <video
+        className="absolute inset-0 w-full h-full object-cover"
+        src="/assets/bg_tree_video_home.mp4"
+        autoPlay
+        loop
+        muted
+        playsInline
+        preload="auto"
+        type="video/mp4"
+        poster="/assets/bg_tree_video_home.jpg"
+        alt="Background Video"
+        title="Background Video"
+        
+      ></video>
+
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-opacity-50"></div>
+
+      {/* Content */}
+      <div className="relative z-10 max-w-4xl">
+        <h1 className="font-knewave text-6xl sm:text-8xl md:text-9xl lg:text-10xl font-bold shadow-md">
+          Welcome <br /> to <br /> DevelopLeaf
+        </h1>
+        <p className="mt-4 text-lg lg:text-4xl font-semibold shadow-md">
+          Join us in making the world greener by planting trees and sharing your
+          journey.
+        </p>
+        <button className="mt-6 shadow-md px-6 lg:px-18 py-3 bg-green-600 hover:bg-green-700 text-white font-semibold rounded-lg shadow-md transition cursor-pointer">
+          Get Started
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
